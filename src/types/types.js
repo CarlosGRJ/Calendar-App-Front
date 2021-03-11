3c9b549c4dfcafa2c576a119902f17c81edfe8cb
CarlosGRJ <carlosgrj@outlook.com>
2021-03-11 14:40:19 -0600

    Eliminar Evento

--- src/types/types.js
+++ src/types/types.js
@@ -1,13 +1,10 @@
-
 export const types = {
+  uiOpenModal: '[ui] Open modal',
+  uiCloseModal: '[ui] Close modal',
 
-    uiOpenModal: '[ui] Open modal',
-    uiCloseModal: '[ui] Close modal',
-
-    eventSetActive: '[event] Set Active',
-    eventAddNew: '[event] Add new',
-    eventClearActiveEvent: '[event] Clear Active Event',
-    eventUpdated: '[event] Event updated'
-
-
-}
+  eventSetActive: '[event] Set Active',
+  eventAddNew: '[event] Add new',
+  eventClearActiveEvent: '[event] Clear Active Event',
+  eventUpdated: '[event] Event updated',
+  eventDeleted: '[event] Event deleted',
+};
